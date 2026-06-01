76ff7e95ba4bae1792ab0a9b1759fe85c803938c
Abhay10-dev <abhayvish10@gmail.com>
2026-06-01 16:56:30 +0530

    fix : added entry/exit message

--- src/org/lazyCoder/Main.java
+++ src/org/lazyCoder/Main.java
@@ -4,9 +4,15 @@ import org.lazyCoder.Services.QuestionServices;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("----------WELCOME TO THE QUIZ GAME----------");
         QuestionServices questionServices = new QuestionServices();
         questionServices.playQuiz();
         questionServices.getScore();
+
+        System.out.print("Your answers : ");
         questionServices.getReply();
+        System.out.println("----------THANK YOU FOR PLAYING THE QUIZ GAME----------");
+
     }
 }
